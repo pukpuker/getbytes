1a596d93cff164b16d5c30204bae4f83db330557
Alexey <gamesense99@mail.ru>
2021-08-18 20:56:49 +0300

    r

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private static bool item;
+
+        static void Main(string[] args)
+        {
+
+
+            byte[] bb = File.ReadAllBytes(@"C:\Users\swordily\Desktop\neverlose\server\Debug\servv.exe");
+            string ss = get(bb);
+            File.WriteAllText("ss.txt", ss);
+
+
+        }
+        public static string get(byte[] vv)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in vv)
+            {
+                sb.Append(item);
+                sb.Append(",");
+            }
+            return sb.ToString().Remove(sb.Length - 1);
+        }
+    }
+}
